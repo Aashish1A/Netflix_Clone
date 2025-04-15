2c3e73a5cf25d8fe33ef2bf194b37cc750fe5401
Aashish Kumar <166286708+Aashish1A@users.noreply.github.com>
2025-04-15 16:38:39 +0530

    Created Sign In and Sign Up for login

--- src/Components/Pages/Login/Login.jsx
+++ src/Components/Pages/Login/Login.jsx
@@ -1,15 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from "../../../assets/logo.png";
 
 const Login = () => {
+
+  const [signState, setSignState] = useState("Sign In")
+
   return (
-    <div className=''>
-        <img src={logo} alt="" className='' />
-        <div className="">
-            <h1>Sign In</h1>
+    <div className="h-screen backgroundImage py-5 px-[8%]">
+        <img src={logo} alt="" className='w-36' />
+        <div className="w-full max-w-md bg-[rgba(0,0,0,0.75)] rounded p-15 m-auto">
+            <h1 className='text-8 font-medium mb-7'>{signState}</h1>
             <form>
-                <input type="text" placeholder='Your name' />
+                {signState==='Sign Up'?<input type="text" placeholder='Your name' className='w-full h-10 bg-[#333] text-white my-3 mx-0 border-0 outline-0 rounded py-4 px-5 text-base font-medium' />:<></>}
+                <input type="email" placeholder='Email' className='w-full h-10 bg-[#333] text-white my-3 mx-0 border-0 outline-0 rounded py-4 px-5 text-base font-medium'  />
+                <input type="password" placeholder='Password' className='w-full h-10 bg-[#333] text-white my-3 mx-0 border-0 outline-0 rounded py-4 px-5 text-base font-medium' />
+                <button className='w-full border-0 outline-0 p-4 bg-[#e50914] rounded text-base font-medium mt-5 cursor-pointer'>{signState}</button>
+                <div className="flex items-center justify-between text-[#b3b3b3] text-[13px]">
+                  <div className="flex items-center gap-1">
+                    <input type="checkbox" className='w-4 h-4' />
+                    <label htmlFor="">Remember Me</label>
+                  </div>
+                  <p>Need Help?</p>
+                </div>
             </form>
+            <div className='mt-10 text-[#737373]'>
+            {signState==='Sign In' ?  <p>New to Netflix? <span className='ml-1.5 text-white font-medium cursor-pointer' onClick={()=>{setSignState("Sign Up")}} >Sign Up Now</span> </p>:<p>Already have account? <span className='ml-1.5 text-white font-medium cursor-pointer' onClick={()=>{setSignState("Sign In")}}>Sign In Now</span> </p>}
+            </div>
         </div>
     </div>
   )
